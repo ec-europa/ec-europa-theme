4f4b52b585fc68fafe3fc4d2a6be1b673da8992c
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-11 16:57:03 +0200

    EUTHEME-197: Upgrade the logo component.

--- templates/logo/logo.tpl.php
+++ templates/logo/logo.tpl.php
@@ -5,6 +5,6 @@
  * Contains template file.
  */
 ?>
-<a href="#top" class="ecl-logo" title="Home">
-    <span class="ecl-sr-only">Home</span>
+<a href="#top" class="ecl-logo" title="<?php print $page_logo_title; ?>">
+    <span class="ecl-sr-only"><?php print $page_logo_title; ?></span>
 </a>
