e4224bb47bf7ac4c890fbc1ebed71cb96a87fc06
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-12 18:30:22 +0200

    EUTHEME-198: Move the search box to components, update related organisms.

---
+++ templates/search_bar/search-bar.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains component file.
+ */
+?>
+<section class="header-search-bar" aria-label="Site tools">
+  <div class="header-search-bar__wrapper container-fluid">
+    <section class="block-nexteuropa-europa-search clearfix">
+      <div class="block__content">
+      <?php print render($search_form); ?>
+    </section>
+    </div>
+  </section>
--- templates/site_header/site-header.tpl.php
+++ templates/site_header/site-header.tpl.php
@@ -14,21 +14,5 @@
         </section>
         <?php print render($logo); ?>
     </div>
-    <section class="header-search-bar" aria-label="Site tools">
-        <div class="header-search-bar__wrapper container-fluid">
-            <section class="block-nexteuropa-europa-search clearfix">
-                <form class="search-form">
-                    <div class="input-group">
-                        <label class="search-form__textfield-wrapper">
-      <span class="ecl-sr-only">Search this website</span>
-      <input type="search" class="search-form__textfield form-control" size="60" maxlength="128" title="Search">
-    </label>
-                        <span class="search-form__btn-wrapper input-group-btn">
-      <button class="search-form__btn btn btn-primary btn-search" value="Search" type="submit">Search</button>
-    </span>
-                    </div>
-                </form>
-            </section>
-        </div>
-    </section>
-</header>
+    <?php print render($search_bar); ?>
+ </header>
