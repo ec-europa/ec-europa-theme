40a3a0303f5d1f683fd669d73fa7cc84a364eb68
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-12 20:35:30 +0200

    EUTHEME-198: Implement logic of the language switcher.

--- templates/lang_select_site/lang-select-site.tpl.php
+++ templates/lang_select_site/lang-select-site.tpl.php
@@ -6,11 +6,11 @@
  */
 ?>
 <div class="lang-select-site clearfix">
-  <a href="#" class="lang-select-site__link">
-    <span class="lang-select-site__label">English</span>
+  <a href="<?php print $url; ?>" class="lang-select-site__link">
+    <span class="lang-select-site__label"><?php print render($label); ?></span>
     <span class="lang-select-site__code">
       <span class="icon icon--language lang-select-site__icon"></span>
-      <span class="lang-select-site__code-text">en</span>
+      <span class="lang-select-site__code-text"><?php print render($code); ?></span>
     </span>
   </a>
 </div>
