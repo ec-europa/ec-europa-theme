e1175b084986d89af3036eff6904e4b2bbcd24e8
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-13 10:00:59 +0200

    EUTHEME-198: Make some changes because the existing page.tpl of DTT is not in line with the styleguide.

--- templates/site_header/site-header.tpl.php
+++ templates/site_header/site-header.tpl.php
@@ -6,13 +6,18 @@
  */
 ?>
 <header class="site-header site-header--homepage" role="banner">
-    <div class="container-fluid site-header__container">
-        <section class="top-bar" aria-label="Site tools">
-          <div class="top-bar__wrapper">
-            <?php print render($lang_select_site); ?>                
-          </div>
-        </section>
-        <?php print render($logo); ?>
-    </div>
-    <?php print render($search_bar); ?>
+  <div class="container-fluid site-header__container">
+    <?php print render($logo); ?>
+    <?php print $site_slogan; ?>
+    <section class="top-bar" aria-label="Site tools">
+      <div>
+        <div class="top-bar__wrapper">
+          <?php print render($lang_select_site); ?>
+          <h1 class="sr-only">Site name</h1>
+        </div>
+      </div>
+    </section>
+  </div>
+  <?php print render($search_bar); ?>
+  <?php print render($page_header); ?>
  </header>
