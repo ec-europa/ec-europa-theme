ed8da9b285dabcf8d0ec6cf527249bf22a46cbd4
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-25 18:01:34 +0200

    EUTHEME-220: Add the inpage_nav component.

---
+++ templates/inpage_nav/inpage_nav.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<section id="heading-<?php echo $counter; ?>">
+  <h2 class="block-title inpage-nav__block-title"><?php echo $title; ?></h2>
+  <?php echo $content; ?>
+</section>
