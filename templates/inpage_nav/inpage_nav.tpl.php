e2ad56320f402e2c9f43a75d06cc961ea5965ef7
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-04-26 13:39:27 +0200

    EUTHEME-220: Add missing render call, fix typo, fix header component and views table.

--- templates/inpage_nav/inpage_nav.tpl.php
+++ templates/inpage_nav/inpage_nav.tpl.php
@@ -6,6 +6,6 @@
  */
 ?>
 <section id="heading-<?php echo $counter; ?>">
-  <h2 class="block-title inpage-nav__block-title"><?php echo $title; ?></h2>
-  <?php echo $content; ?>
+  <h2 class="block-title inpage-nav__block-title"><?php echo render($title); ?></h2>
+  <?php echo render($content); ?>
 </section>
