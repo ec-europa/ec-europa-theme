a79ff96496b7eac896483e22a7fb43285065f3e5
planctus <davidepuntoferrante@gmail.com>
2017-06-12 15:03:21 +0200

    EUTHEME-297 - Adding the setting for choosing between two different headers

--- theme-settings.php
+++ theme-settings.php
@@ -17,18 +17,44 @@ function europa_form_system_theme_settings_alter(&$form, &$form_state) {
     ];
   }
 
-  $form['europa']['ec_europa_improved_website'] = [
+  $form['europa']['improved'] = [
+    '#type' => 'fieldset',
+    '#title' => t('Europa settings about improved websites'),
+  ];
+
+  $form['europa']['improved']['ec_europa_improved_website'] = [
     '#type' => 'checkbox',
     '#title' => t('Is this an "improved website"?'),
     '#description' => t('If this website is an "improved" one, we are going to show a site identification element in the page header.'),
     '#default_value' => theme_get_setting('ec_europa_improved_website', 'europa'),
+    '#weight' => 0,
   ];
 
-  $form['europa']['ec_europa_improved_website_home'] = [
+  $form['europa']['improved']['ec_europa_improved_website_header'] = [
+    '#type' => 'radios',
+    '#title' => t('Will you use the "basic" page header or the "complete" one?'),
+    '#description' => t('You can choose between two different layouts for your page header, see <a href="@here">here</a>', ['@here' => 'https://ec-europa.github.io/ec-europa-theme/section-page-header.html']),
+    '#default_value' => theme_get_setting('ec_europa_improved_website_header', 'europa'),
+    '#options' => [
+      'basic' => t('Basic'),
+      'complete' => t('Complete'),
+    ],
+    '#weight' => 0,
+    '#states' => [
+      'visible' => [
+        ':input[name="ec_europa_improved_website"]' => [
+          'checked' => TRUE,
+        ],
+      ],
+    ],
+  ];
+
+  $form['europa']['improved']['ec_europa_improved_website_home'] = [
     '#type' => 'checkbox',
     '#title' => t('Do you want to show the site identification also in the home page?'),
     '#description' => t('The site identification could duplicate your page title in homepage, you can hide it.'),
     '#default_value' => theme_get_setting('ec_europa_improved_website_home', 'europa'),
+    '#weight' => 0,
     '#states' => [
       'visible' => [
         ':input[name="ec_europa_improved_website"]' => [
