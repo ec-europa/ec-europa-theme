24081d296b2369dc8b89fff265898f4d7d6f9f48
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-06-16 15:05:06 +0200

    NEPT-1059: Modify the breadcrumb classes to match ecl.

--- templates/easy_breadcrumb/easy-breadcrumb.tpl.php
+++ templates/easy_breadcrumb/easy-breadcrumb.tpl.php
@@ -6,10 +6,10 @@
  */
 ?>
 <div style="background-color: #004494; padding: 5px 15px 15px; float: left; width: 100%;">
-<nav id="breadcrumb" class="breadcrumb" role="navigation" aria-label="breadcrumbs">
-  <span class="element-invisible"><?php echo t('You are here'); ?></span>
-  <ol class="breadcrumb__segments-wrapper">
-    <?php print render($easy_breadcrumb); ?>
-  </ol>
-</nav>
+  <nav class="ecl-breadcrumbs" aria-label="breadcrumbs">
+    <span class="ecl-sr-only"><?php echo $here; ?></span>
+    <ol class="ecl-breadcrumbs__segments-wrapper">
+      <?php print render($easy_breadcrumb); ?>
+    </ol>
+  </nav>
 </div>
