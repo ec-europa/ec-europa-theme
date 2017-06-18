eb312f4f8976dbd3ce5de90871f288d36dd2144b
Gilles Deudon <gilles.deudon@atos.net>
2017-06-18 09:57:59 +0200

    NEPT-970: Stabilize grid & search-bar theme to render properly in atomium-overview page.

--- templates/grid/grid.tpl.php
+++ templates/grid/grid.tpl.php
@@ -12,11 +12,17 @@
     <caption><?php print $caption; ?></caption>
 <?php endif; ?>
 
-<div class="ecl-container <?php print $class; ?>"<?php print $attributes; ?>>
+<div <?php print drupal_attributes($attributes_array); ?>>
     <?php foreach ($rows as $row_number => $columns): ?>
-        <div class="ecl-row <?php print $row_classes[$row_number];?> ">
+        <div class="ecl-row <?php
+        if (isset($row_classes[$row_number])) {
+          print $row_classes[$row_number];
+        }?> ">
           <?php foreach ($columns as $column_number => $item): ?>
-              <div class="ecl-col <?php print $column_classes[$row_number][$column_number];?> ">
+              <div class="ecl-col <?php
+                 if (isset($column_classes[$row_number][$column_number])) {
+                   print $column_classes[$row_number][$column_number];
+                 } ?> ">
                 <?php print $item; ?>
               </div>
           <?php endforeach; ?>
