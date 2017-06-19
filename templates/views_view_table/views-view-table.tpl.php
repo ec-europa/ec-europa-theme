7f4a566e692b48b2eff4552b11a1c2a52500e279
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-06-19 14:26:08 +0200

    EUTHEME-84: Update Views View Table and move logic to the preprocess/process functions.

--- templates/views_view_table/views-view-table.tpl.php
+++ templates/views_view_table/views-view-table.tpl.php
@@ -19,10 +19,7 @@
  * @ingroup views_templates
  */
 ?>
-<table <?php
-  if ($classes){
-   print 'class="' . $classes . '" ';
-  } ?><?php print $attributes; ?>>
+<table<?php print $attributes; ?>>
   <?php if (!empty($title) || !empty($caption)) : ?>
     <caption><?php print $caption . $title; ?></caption>
   <?php endif; ?>
@@ -30,27 +27,18 @@
     <thead>
     <tr>
       <?php foreach ($header as $field => $label): ?>
-        <th <?php
-        if ($header_classes[$field]){
-          print 'class="' . $header_classes[$field] . '" ';
-        } ?> scope="col">
-          <?php print $label; ?>
-        </th>
+          <th<?php print $header_attributes[$field]; ?>>
+            <?php print $label; ?>
+          </th>
       <?php endforeach; ?>
     </tr>
     </thead>
   <?php endif; ?>
   <tbody>
   <?php foreach ($rows as $row_count => $row): ?>
-    <tr <?php
-    if ($row_classes[$row_count]){
-      print 'class="' . implode(' ', $row_classes[$row_count]) . '"';
-    } ?>>
+    <tr<?php print $row_attributes[$row_count]; ?>>
       <?php foreach ($row as $field => $content): ?>
-        <td <?php
-        if ($field_classes[$field][$row_count]){
-          print 'class="' . $field_classes[$field][$row_count] . '" ';
-        } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+        <td<?php print $column_attributes[$row_count][$field]; ?>>
           <?php print $content; ?>
         </td>
       <?php endforeach; ?>
