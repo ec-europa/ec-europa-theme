d7dfc7f53d2ae629aec22e4252769dd61cb26cde
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-06-19 11:32:56 +0200

    EUTHEME-84: Update Grid component and move logic to the preprocess/process functions.

--- templates/grid/grid.tpl.php
+++ templates/grid/grid.tpl.php
@@ -5,6 +5,7 @@
  * Contains template file.
  */
 ?>
+
 <?php if (!empty($title)) : ?>
     <h3><?php print $title; ?></h3>
 <?php endif; ?>
@@ -12,20 +13,14 @@
     <caption><?php print $caption; ?></caption>
 <?php endif; ?>
 
-<div <?php print drupal_attributes($attributes_array); ?>>
-    <?php foreach ($rows as $row_number => $columns): ?>
-        <div class="ecl-row <?php
-        if (isset($row_classes[$row_number])) {
-          print $row_classes[$row_number];
-        }?> ">
-          <?php foreach ($columns as $column_number => $item): ?>
-              <div class="ecl-col <?php
-                 if (isset($column_classes[$row_number][$column_number])) {
-                   print $column_classes[$row_number][$column_number];
-                 } ?> ">
-                <?php print $item; ?>
-              </div>
-          <?php endforeach; ?>
-        </div>
-    <?php endforeach; ?>
- </div>
+<div<?php print $attributes; ?>>
+  <?php foreach ($rows as $row_number => $columns): ?>
+      <div<?php print $row_attributes[$row_number]; ?>>
+        <?php foreach ($columns as $column_number => $item): ?>
+            <div<?php print $column_attributes[$row_number][$column_number]; ?>>
+              <?php print $item; ?>
+            </div>
+        <?php endforeach; ?>
+      </div>
+  <?php endforeach; ?>
+</div>
