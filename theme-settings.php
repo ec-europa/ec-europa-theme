c7b9b584ce45e98a7dbe6a794fd0b6c328aac382
Nelson Matias <grin.correio@gmail.com>
2017-06-19 20:26:18 +0200

    EUTHEME-299: Changed some text.

--- theme-settings.php
+++ theme-settings.php
@@ -25,14 +25,14 @@ function europa_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['europa']['improved']['ec_europa_improved_website'] = [
     '#type' => 'checkbox',
     '#title' => t('Is this an "improved website"?'),
-    '#description' => t('If this website is an "improved" one, we are going to show a site identification element in the page header.'),
+    '#description' => t('If the website is an "improved" one, it will display a site identification element in the page header.'),
     '#default_value' => theme_get_setting('ec_europa_improved_website', 'europa'),
     '#weight' => 0,
   ];
 
   $form['europa']['improved']['ec_europa_improved_identification'] = [
     '#type' => 'checkbox',
-    '#title' => t('Do you want to show the site identification element?'),
+    '#title' => t('Do you want to display the site identification element?'),
     '#description' => t('You can find a demo of the site identification <a href="@here">in the Europa styleguide</a>', ['@here' => 'https://ec-europa.github.io/ec-europa-theme/section-page-header.html#kssref-page-header-4']),
     '#default_value' => theme_get_setting('ec_europa_improved_identification', 'europa'),
     '#weight' => 0,
@@ -40,7 +40,7 @@ function europa_form_system_theme_settings_alter(&$form, &$form_state) {
 
   $form['europa']['improved']['ec_europa_improved_website_home'] = [
     '#type' => 'checkbox',
-    '#title' => t('Do you want to show the site identification also in the home page?'),
+    '#title' => t('Do you want to display the site identification also in the home page?'),
     '#description' => t('The site identification could duplicate your page title in homepage, you can hide it.'),
     '#default_value' => theme_get_setting('ec_europa_improved_website_home', 'europa'),
     '#weight' => 0,
