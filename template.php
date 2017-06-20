fdd915a9dc98a8abff640842788984d0a9b248d7
planctus <davidepuntoferrante@gmail.com>
2017-06-20 10:16:12 +0200

    EUTHEME-308 - Replacing unset with hide

--- template.php
+++ template.php
@@ -1065,7 +1065,7 @@ function europa_preprocess_taxonomy_term(&$variables) {
 function europa_preprocess_page(&$variables) {
   // Disable the region holding the breadcrumb if settings say so.
   if (drupal_is_front_page() && !(theme_get_setting('ec_europa_breadcrumb_home'))) {
-    unset($variables['page']['header_bottom']);
+    hide($variables['page']['header_bottom']);
   }
 
   // Small fix to maxe the link to the start page use the alias with language.
