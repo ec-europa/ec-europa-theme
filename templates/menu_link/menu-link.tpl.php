bae56b19f81b0ce7c71c0631cfa0fab685308ef1
Imanol Eguskiza <ier@opendeusto.es>
2017-06-23 12:10:24 +0200

    NEPT-1125: First draft

---
+++ templates/menu_link/menu-link.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<li<?php print $attributes; ?>><?php print render($output); ?><?php print render($sub_menu); ?></li>
---
+++ templates/menu_tree/menu-tree.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<ul<?php print $attributes; ?>><?php print render($tree); ?></ul>
