28b75ffef568745a6568b9046c3a535af5f5723a
Imanol Eguskiza <ier@opendeusto.es>
2017-06-26 10:41:46 +0200

    NEPT-1125: Added JS

--- templates/menu_tree/menu-tree--main-menu.tpl.php
+++ templates/menu_tree/menu-tree--main-menu.tpl.php
@@ -9,7 +9,7 @@
   <nav class="ecl-navigation-menu" aria-label="Main Navigation">
     <div class="ecl-container">
       <div class="ecl-row">
-        <button class="ecl-navigation-menu__toggle ecl-navigation-menu__hamburger ecl-navigation-menu__hamburger--squeeze" aria-controls="nav-menu-expandable-root" aria-expanded="true" id="nav-menu-expandable-toggle">
+        <button class="ecl-navigation-menu__toggle ecl-navigation-menu__hamburger ecl-navigation-menu__hamburger--squeeze" aria-controls="nav-menu-expandable-root" aria-expanded="false" id="nav-menu-expandable-toggle">
           <span class="ecl-navigation-menu__hamburger-box">
             <span class="ecl-navigation-menu__hamburger-inner"></span>
           </span>
