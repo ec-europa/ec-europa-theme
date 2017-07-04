055953fa1169ff2dbd4ca82e8ce6fb114e30972a
Vitor DA COSTA <vitor.da-costa@ext.ec.europa.eu>
2017-07-04 13:04:36 +0200

    NEPT-1059: Remove the breadcrumb tpl not used, remove breadcrum.js not used and remove hard coding in the easy-breadcrumb tpl

--- templates/easy_breadcrumb/easy-breadcrumb.tpl.php
+++ templates/easy_breadcrumb/easy-breadcrumb.tpl.php
@@ -5,11 +5,9 @@
  * Contains template file.
  */
 ?>
-<div style="background-color: #004494; padding: 5px 15px 15px; float: left; width: 100%;">
-  <nav class="ecl-breadcrumbs" aria-label="breadcrumbs">
-    <span class="ecl-sr-only"><?php echo $here; ?></span>
-    <ol class="ecl-breadcrumbs__segments-wrapper">
-      <?php print render($easy_breadcrumb); ?>
-    </ol>
-  </nav>
-</div>
+<nav class="ecl-breadcrumbs" aria-label="breadcrumbs">
+  <span class="ecl-sr-only"><?php print render($here); ?></span>
+  <ol class="ecl-breadcrumbs__segments-wrapper">
+    <?php print render($easy_breadcrumb); ?>
+  </ol>
+</nav>
