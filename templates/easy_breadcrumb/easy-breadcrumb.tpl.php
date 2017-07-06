eabe4a6cb8204a54d0062e880e550feff33801b1
Vitor DA COSTA <vitor.da-costa@ext.ec.europa.eu>
2017-07-06 19:33:55 +0200

    NEPT-1059: Add the extraClass to the tpl

--- templates/easy_breadcrumb/easy-breadcrumb.tpl.php
+++ templates/easy_breadcrumb/easy-breadcrumb.tpl.php
@@ -5,7 +5,7 @@
  * Contains template file.
  */
 ?>
-<nav class="ecl-breadcrumbs" aria-label="breadcrumbs">
+<nav class="ecl-breadcrumbs <?php print render($extra_class); ?>" aria-label="breadcrumbs">
   <span class="ecl-sr-only"><?php print render($here); ?></span>
   <ol class="ecl-breadcrumbs__segments-wrapper">
     <?php print render($easy_breadcrumb); ?>
