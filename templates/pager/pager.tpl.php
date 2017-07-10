2eef7d539356de84acf37d2bc9d487b5d4159bdd
Vitor DA COSTA <vitor.da-costa@ext.ec.europa.eu>
2017-07-10 14:38:49 +0200

    NEPT-1086: Put all titles and labels in the preprocess.

--- templates/pager/pager.tpl.php
+++ templates/pager/pager.tpl.php
@@ -11,7 +11,7 @@
 
     <?php if (isset($links['pager_previous'])): ?>
       <li class="ecl-pager__item ecl-pager__item--previous">
-        <a class="ecl-pager__link" title="<?php t('Go to previous page') ?>" href="<?php print $links['pager_previous']['url'] ?>">‹ <?php print t('Previous')?></a>
+        <a class="ecl-pager__link" title="<?php print $previous_title ?>" href="<?php print $links['pager_previous']['url'] ?>"><?php print $previous_label ?></a>
       </li>
     <?php endif ?>
 
@@ -19,7 +19,7 @@
 
       <?php if (($pager_last - $quantity >= 1) && $cursor == $pager_first): ?>
         <li class="ecl-pager__item ecl-pager__item--first">
-          <a class="ecl-pager__link" title="<?php t('Go to page !page', array('!page' => 1)) ?>" href="<?php print $links['pager_first']['url'] ?>">1</a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> 1" href="<?php print $links['pager_first']['url'] ?>">1</a>
         </li>
         <?php if ($pager_last - $quantity > 1): ?>
           <li class="ecl-pager__item ecl-pager__item--ellipsis">…</li>
@@ -27,17 +27,17 @@
       <?php endif ?>
 
       <?php if ($cursor == $pager_current): ?>
-        <li class="ecl-pager__item ecl-pager__item--current"><span class="ecl-pager__item-text"><?php print t('Page') ?></span> <?php print $pager_current ?></li>
+        <li class="ecl-pager__item ecl-pager__item--current"><span class="ecl-pager__item-text"><?php print $page_label ?></span> <?php print $pager_current ?></li>
       <?php else: ?>
         <li class="ecl-pager__item">
-          <a class="ecl-pager__link" title="<?php t('Go to page !page', array('!page' => $links['pager_link__' . $cursor]['label'])) ?>" href="<?php print $links['pager_link__' . $cursor]['url'] ?>"><?php print $links['pager_link__' . $cursor]['label'] ?></a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $links['pager_link__' . $cursor]['label'] ?>" href="<?php print $links['pager_link__' . $cursor]['url'] ?>"><?php print $links['pager_link__' . $cursor]['label'] ?></a>
         </li>
       <?php endif ?>
 
       <?php if (($pager_first + $quantity < $pager_max) && $cursor == $pager_last): ?>
         <li class="ecl-pager__item ecl-pager__item--ellipsis">…</li>
         <li class="ecl-pager__item ecl-pager__item--last">
-          <a class="ecl-pager__link" title="<?php t('Go to page !page', array('!page' => $pager_max)) ?>" href="<?php print $links['pager_last']['url'] ?>"><?php print $pager_max ?></a>
+          <a class="ecl-pager__link" title="<?php print $goto_title ?> <?php print $pager_max ?>" href="<?php print $links['pager_last']['url'] ?>"><?php print $pager_max ?></a>
         </li>
       <?php endif ?>
 
@@ -45,7 +45,7 @@
 
     <?php if (isset($links['pager_next'])): ?>
       <li class="ecl-pager__item ecl-pager__item--next">
-        <a class="ecl-pager__link" title="<?php t('Go to next page') ?>" href="<?php print $links['pager_next']['url'] ?>"><?php print t('Next') ?> ›</a>
+        <a class="ecl-pager__link" title="<?php print $next_title ?>" href="<?php print $links['pager_next']['url'] ?>"><?php print $next_label ?></a>
       </li>
     <?php endif ?>
   </ul>
