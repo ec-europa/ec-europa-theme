cc0506481e7f0312ee944b43ab392e5220e17033
Kalin Chernev <kalin.chernev@gmail.com>
2017-07-11 13:23:15 +0200

    NEPT-1171: Remove unnecessary dependencies.

--- assets/js/entry.js
+++ assets/js/entry.js
@@ -1,19 +1,9 @@
-// const path = require("path");
-// This logic for finding the path to the js files.
-// const source_folder = __dirname + "/node_modules/@ec-europa";
-
+// Taking modueles from ECL.
 export * from "../../node_modules/@ec-europa/ecl-accordions/accordions";
 export * from "../../node_modules/@ec-europa/ecl-dropdowns/ecl-dropdowns";
-export * from "../../node_modules/ecl-expandables/expandables";
-
-// export * from '../../node_modules/ecl-files/files';
-// export * from '../../node_modules/ecl-filters/filters';
-// export * from '../../node_modules/ecl-forms/ecl-forms-file-uploads/ecl-forms-file-uploads';
-// export * from '../../node_modules/ecl-inpage-navs/inpage-navs';
-
-export * from "../../node_modules/ecl-messages/messages";
-export * from "../../node_modules/ecl-navigation/ecl-navigation-menus/megamenu";
-export * from "../../node_modules/ecl-tables/ecl-tables";
-export * from "../../node_modules/ecl-tabs/tabs";
-
-// export * from '../../node_modules/ecl-timelines/timelines';
+export * from "../../node_modules/@ec-europa/ecl-expandables/expandables";
+export * from "../../node_modules/@ec-europa/ecl-forms/ecl-forms-file-uploads/ecl-forms-file-uploads";
+export * from "../../node_modules/@ec-europa/ecl-messages/messages";
+export * from "../../node_modules/@ec-europa/ecl-navigation/ecl-navigation-menus/megamenu";
+export * from "../../node_modules/@ec-europa/ecl-tables/ecl-tables";
+export * from "../../node_modules/@ec-europa/ecl-tabs/tabs";
